86e180d1948348dd662122c64e15bc7fc67e6338
bmullen90 <43555885+bmullen90@users.noreply.github.com>
2018-10-24 17:28:18 -0600

    testing the branch
    making sure that pickle.php just goes to my branch

---
+++ pickle.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link rel="stylesheet" href="CSS/green.css">
+</head>
+
+<body>
+	<strong>Tart and tangy! and kinda gross</strong>
+	
+</body>
+</html>
